70f0c7cf1044cd86ee31c7dae90386abda80f60b
Martín Ramonda <mramonda@orbitas.es>
2025-04-13 21:15:06 +0200

    añadido side menu y eventos toggle

--- js/main.js
+++ js/main.js
@@ -1,7 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     const burgerMenu = document.getElementById('burger-menu-top');
     burgerMenu.addEventListener('click', () => {
-        console.log('click');
+        const sideMenu = document.getElementById('onoff-side-menu');
         burgerMenu.classList.toggle('is-active');
+        sideMenu.classList.toggle('active');
+    });
+
+    const svg = document.querySelectorAll('.slim-side-menu svg');
+    const sectionDisplay = document.querySelector(".onoff-section-display");
+    const sections = sectionDisplay.querySelectorAll('div');
+    let sectionPositions = [];
+    svg.forEach((el, index) => {
+        el.addEventListener('mouseenter', () => {
+            if(sectionPositions.length === 0){
+                sectionPositions = Array.from(sections).map(sec => sec.offsetTop);
+            }
+            svg.forEach(el => el.classList.add('toggled'));
+            el.classList.toggle('toggled');
+            let pos = 0;
+            if(index > 0){
+                pos = el.parentElement.offsetTop - sectionPositions[index];
+            }
+            sectionDisplay.style.transform = `translateY(${pos}px)`;
+        });
+    });
+
+    sections.forEach((section, index) => {
+        section.addEventListener('mouseenter', () => {
+            if(sectionPositions.length === 0){
+                sectionPositions = Array.from(sections).map(sec => sec.offsetTop);
+            }
+            svg.forEach(el => el.classList.add('toggled'));
+            svg[index].classList.toggle('toggled');
+            let pos = 0;
+            if(index > 0){
+                pos = svg[index].parentElement.offsetTop - sectionPositions[index];
+            }
+            sectionDisplay.style.transform = `translateY(${pos}px)`;
+        });
     });
 });
